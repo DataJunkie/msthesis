58ac205c0e02578eb9a9c236d00fcca7edb2d81d
Ryan R. Rosario <ryanrosario@id-55-74.stat.ucla.edu>
2010-07-11 21:56:19 -0700

    Routine commit

--- pickler.py
+++ pickler.py
@@ -8,11 +8,12 @@ print "Building graph..."
 G = BuildGraph()
 print "Converting graph to matrix..."
 A = nx.to_scipy_sparse_matrix(G, dtype="bool")
-print "Computing transpose..."
-intersection_matrix = A.transpose() * A
-print "Converting to CSC type..."
+#print "Computing transpose..."
+intersection_matrix = A * A.transpose()
+#print "Converting to CSC type..."
 intersection_matrix = intersection_matrix.tocsr()
-print "Dumping pickle to disk..."
+A = A.tocsr()
+#print "Dumping pickle to disk..."
 #scipy.io.savemat("intersection.pickle", {'matrix': intersection_matrix})
-nnz = jaccard.no_nonzeros(intersection_matrix, axis=1, pickle=True, disk=False, nnzfile="")
+nnz = jaccard.no_nonzeros(A, axis=1, pickle=True, disk=False, nnzfile="")
 print "Done."
